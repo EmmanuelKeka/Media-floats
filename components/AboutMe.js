955880222678e4be20741e4b6c66f18f3b74a49e
Emmanuel Keka <72016068+EmmanuelKeka@users.noreply.github.com>
2023-01-11 20:23:38 +0000

    safari font fixed

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -5,21 +5,19 @@ export default function AboutMe(){
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1320 290" width="100%" height="100%">
                 <path fill="#00AEEF" fill-opacity="1" d="M0 192 C220 100 440 100 660 192 C880 290 1100 290 1320 192 L1320 300 L0 300" width="100%" height="100%"></path>
             </svg>
-            <section className="blue-back text-center flex flex flex-col items-center">
+            <section className="blue-back text-center flex flex-col items-center">
                 <h1 className="label-font tittle white--text text-5xl mt-4 drop-shadow-lg shadow-black">About me</h1>
-                <div className=" flex flex-row justify-between flex-wrap text-5xl max-w-4x w-full lg:flex-col lg:items-center">
-                    <div className="bg-white w-[14rem] h-[14rem] liquidBubble overflow-hidden m-[1rem] shadow-xl lg:w-[12rem] lg:h-[12rem]">
-                        <div className='pt-1'>
+                <div className=" flex flex-row justify-between  flex-wrap text-5xl max-w-4x w-full lg:flex-col lg:items-center">
+                    <div className="bg-white w-[20rem] h-[20rem] liquidBubble overflow-hidden m-[1rem] shadow-xl lg:w-[12rem] lg:h-[12rem]">
+                        <div className='pt-6'>
                         <Image src="/emmalove.png" alt="emma"  height="300" width="300" priority/>
                         </div>
                     </div>
-                    <div className="h-[16rem] w-[25rem] lg:h-[30rem] lg:w-[14rem] p-0 text-lg text-left m-[1rem] lg:w-[12rem] lg:h-[33rem]">
-                        <p className="para--font text-center">
-                            My name is Emmanuel Keka, I am a web developer located in Ireland, Dublin. 
-                            I love anime, cars and everything that has to do with technology and science.
-                            I am very passionate about software development and love to see my ideas come to life.
-                            Let me also help you to bring your ideas and dreams to life by providing you with a 
-                            beautiful website, i am looking forward to working with you.
+                    <div className="bg-white rounded-xl p-5 m-[1rem]  h-[21rem] w-[20rem] shadow-xl lg:h-[30rem] lg:w-[14rem] p-0 text-lg text-left m-[1rem] lg:w-[12rem] lg:h-[33rem]">
+                        <p className="para--font text-center text-black">
+                        Hi, I am Emmanuel Keka. I love anime, cars, and everything that has to do with technology and science. 
+                        I am very passionate about software development and love to see my ideas come to life. Let me also help you bring your 
+                        ideas and dreams to life by providing you with a beautiful website. I am looking forward to working with you.
                         </p>
                     </div>
                 </div>
--- components/Hero.js
+++ components/Hero.js
@@ -8,21 +8,21 @@ export default function Hero(){
             <p className="white--para para--font text-lg"> we deliver wesite on all categories such as: </p>
             <div className="mt-[4rem] flex flex-row flex-wrap max-w-4x w-full justify-between lg:flex-col lg:items-center">
                 <div className="m-[1rem] ">
-                <div className="web--suj blue-back liquidBubbl--two w-[10rem] h-[10rem] shadow-xl lg:w-[12rem] lg:h-[12rem] mb-5">
+                <div className="web--suj blue-back liquidBubbl--two w-[8rem] h-[8rem] shadow-xl lg:w-[12rem] lg:h-[12rem] mb-5">
                 <img src="/music.png" alt="music" className=" w-[85%]"/>
                 </div>
                 <span className="white--para para--font font-semibold mt-2">MUSIC BLOG</span>
                 </div>
 
                 <div className="m-[1rem]">
-                <div className="web--suj blue-back liquidBubbl--tree w-[10rem] h-[10rem] shadow-xl lg:w-[12rem] lg:h-[12rem] mb-5">
+                <div className="web--suj blue-back liquidBubbl--tree w-[8rem] h-[8rem] shadow-xl lg:w-[12rem] lg:h-[12rem] mb-5">
                 <img src="/profolio.png" alt="music" className=" w-[85%]"/>
                 </div>
                 <span className="white--para para--font font-semibold">Profolio</span>
                 </div>
 
                 <div className="m-[1rem]">
-                <div className="web--suj blue-back liquidBubble w-[10rem] h-[10rem] shadow-xl lg:w-[12rem] lg:h-[12rem] mb-5">
+                <div className="web--suj blue-back liquidBubble w-[8rem] h-[8rem] shadow-xl lg:w-[12rem] lg:h-[12rem] mb-5">
                 <img src="/e-commerce.png" alt="music" className="pt-5 w-[85%]"/>
                 </div>
                 <span className="white--para para--font font-semibold">E-commerce</span>
