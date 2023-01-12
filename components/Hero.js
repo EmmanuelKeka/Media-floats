0bcd8e8b639475bd0c75346d2233b70679d93605
Emmanuel Keka <72016068+EmmanuelKeka@users.noreply.github.com>
2023-01-12 02:57:31 +0000

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -7,25 +7,25 @@ export default function Hero(){
             by make your media float to your public</p>
             <p className="white--para para--font text-lg"> we deliver wesite on all categories such as: </p>
             <div className="mt-[4rem] flex flex-row flex-wrap max-w-4x w-full justify-between">
-                <div className="m-[1rem] lg:w-[5rem]">
-                <div className="web--suj blue-back liquidBubbl--two shadow-xl mb-5">
+                <div className="lg:w-[5rem] w-[9rem]">
+                <div className="web--suj blue-back liquidBubbl--two shadow-xl">
                 <img src="/music.png" alt="music" className=" w-[85%]"/>
                 </div>
-                <span className="white--para para--font font-semibold mt-2 lg:text-[53%] ">MUSIC BLOG</span>
+                <span className="white--para para--font font-semibold mt-2 lg:text-[70%] ">MUSIC BLOG</span>
                 </div>
 
-                <div className="m-[1rem] lg:w-[5rem]">
-                <div className="web--suj blue-back liquidBubbl--tree shadow-xl  mb-5">
+                <div className="lg:w-[5rem]  w-[9rem]">
+                <div className="web--suj blue-back liquidBubbl--tree shadow-xl">
                 <img src="/profolio.png" alt="music" className=" w-[85%]"/>
                 </div>
-                <span className="white--para para--font para--font--tittle font-semibold lg:text-[53%]">Profolio</span>
+                <span className="white--para para--font para--font--tittle font-semibold lg:text-[70%]">Profolio</span>
                 </div>
 
-                <div className="m-[1rem] lg:w-[5rem]">
-                <div className="web--suj blue-back liquidBubble shadow-xl  mb-5">
+                <div className="lg:w-[5rem]  w-[9rem]">
+                <div className="web--suj blue-back liquidBubble shadow-xl ">
                 <img src="/e-commerce.png" alt="music" className="pt-1 w-[85%]"/>
                 </div>
-                <span className="white--para para--font font-semibold lg:text-[53%]" >E-commerce</span>
+                <span className="white--para para--font font-semibold lg:text-[70%]" >E-commerce</span>
                 </div>
             </div>
         </section>
