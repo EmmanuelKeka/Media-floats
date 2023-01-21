25fafcdacf840739b6e28adf0e08158936bd891a
Emmanuel Keka <72016068+EmmanuelKeka@users.noreply.github.com>
2023-01-21 20:51:26 +0000

    Update AboutMe.js

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -2,25 +2,7 @@ import Image from "next/image"
 export default function AboutMe(){
     return(
         <div className=" flex flex-col" id="aboutMe">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1320 290" width="100%" height="100%">
-                <path id="orange-circle"  fill="#00AEEF" fill-opacity="1" d="M0 192 C220 100 440 100 660 192 C880 290 1100 290 1320 192 L1320 300 L0 300" width="100%" height="100%"></path>
-            </svg>
-            <section className="blue-back text-center flex flex-col items-center">
-                <h1 className="floating label-font tittle white--text text-5xl mt-4 drop-shadow-lg shadow-black">About me</h1>
-                <div className=" flex flex-col max-w-4x w-full flex-col items-center">
-                    <div className=" bg-white w-[16rem] h-[16rem] liquidBubble overflow-hidden shadow-xl lg:w-[14rem] lg:h-[14rem]">
-                        <div className="pt-6">
-                        <Image className="floating2" src="/image/emmalove.png" alt="emma"  height="300" width="300" priority/>
-                        
-                        </div>
-                    </div>
-                    <div className="floating3 bg-white rounded-xl py-4 h-[14rem] w-[22rem] text-center shadow-xl lg:p-[0.5rem] lg:h-[29rem] lg:w-[14rem] p-0 text-lg text-left m-[1rem] lg:w-[12rem] lg:h-[33rem]">
-                        <p className="para--font text-black pl-4 pr-4">
-                        
-                        </p>
-                    </div>
-                </div>
-            </section>
+        
         </div>
     )
 }
