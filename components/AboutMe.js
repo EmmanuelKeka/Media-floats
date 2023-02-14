7f37c2185a113522f2a32dc7c3253f5ed8124bba
Emmanuel Keka <72016068+EmmanuelKeka@users.noreply.github.com>
2023-02-14 23:51:30 +0000

    Update AboutMe.js

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -9,16 +9,19 @@ export default function AboutMe(){
             <section className="blue-back text-center flex flex-col items-center">
                 <h1 className="floating label-font tittle white--text text-5xl mt-4 drop-shadow-lg shadow-black">About me</h1>
                 <div className=" flex flex-col max-w-4x w-full flex-col items-center">
-                    <div className=" bg-white w-[16rem] h-[16rem] liquidBubble overflow-hidden shadow-xl lg:w-[8rem] lg:h-[8rem]">
-                        <div className="pt-6">
+
+
+                        <div className="pt-6 bg-white w-[16rem] h-[16rem] liquidBubble overflow-hidden shadow-xl lg:w-[8rem] lg:h-[8rem]">
                         <Image className="" src="/image/emmalove.png" alt="emma"  height="300" width="300" priority/>
                         </div>
-                    </div>
+
+
                     <div className="floating3 bg-white rounded-xl py-4 h-[14rem] w-[22rem] text-center shadow-xl lg:p-[0.5rem] lg:h-[23.5rem] lg:w-[14rem] p-0 text-lg text-left m-[1rem] lg:w-[12rem] lg:h-[33rem]">
                         <p className="para--font text-black lg:text-base pl-4 pr-4">
                         {" As a software developer, my passion lies in creating innovative and impactful applications. Allow me to assist you in bringing your unique vision to life, and let's work together to create something truly inspiring and game-changing. I am excited to be a part of this journey and to see your ideas come to fruition. Let's turn your dreams into a reality."}
                         </p>
                     </div>
+                
                 </div>
             </section>
         </div>
