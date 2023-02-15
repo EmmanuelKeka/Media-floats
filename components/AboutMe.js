b80e5399d1b09d73b462998703503e0d9419ff73
Emmanuel Keka <72016068+EmmanuelKeka@users.noreply.github.com>
2023-02-15 14:17:22 +0000

    Update AboutMe.js

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -7,45 +7,40 @@ import { useWebAnimations } from '@wellyshen/use-web-animations';
 export default function AboutMe(){
     const svgRef = useRef(null);
     const WaveAnimation = () => {
-    
-      
-        const wavePathKeyframes = keyframes`
-          0% {
-            d: path("M0 192 C220 100 440 100 660 192 C880 290 1100 290 1320 192 L1320 300 L0 300");
-          }
-          25% {
-            d: path("M0 100 C220 100 440 292 660 292 C880 292 1100 100 1320 100 L1320 300 L0 300");
-          }
-          50% {
-            d: path("M0 192 C220 290 440 290 660 192 C880 100 1100 100 1320 192 L1320 300 L0 300");
-          }
-          75% {
-            d: path("M0 292 C220 292 440 100 660 100 C880 100 1100 292 1320 100 L1320 300 L0 300");
-          }
-          100% {
-            d: path("M0 192 C220 100 440 100 660 192 C880 290 1100 290 1320 192 L1320 300 L0 300");
-          }
-        `;
-      
-        const animationOptions = {
-          keyframes: wavePathKeyframes,
-          timing: {
-            duration: 12000,
-            iterations: 'infinite',
-          },
-        };
-      
-        const { ref, getAnimation } = useWebAnimations({
-          ref: svgRef,
-          ...animationOptions,
-        });
       
         useEffect(() => {
-          const animation = getAnimation();
-          if (animation) {
-            animation.play();
-          }
-        }, [getAnimation]);
+          const timeline = anime.timeline({
+            targets: svgRef.current.querySelectorAll("path"),
+            duration: 8000,
+            easing: "linear",
+            loop: true
+          });
+      
+          timeline.add({
+            d: [
+              {
+                value:
+                  "M0 192 C220 100 440 100 660 192 C880 290 1100 290 1320 192 L1320 300 L0 300"
+              },
+              {
+                value:
+                  "M0 100 C220 100 440 292 660 292 C880 292 1100 100 1320 100 L1320 300 L0 300"
+              },
+              {
+                value:
+                  "M0 192 C220 290 440 290 660 192 C880 100 1100 100 1320 192 L1320 300 L0 300"
+              },
+              {
+                value:
+                  "M0 292 C220 292 440 100 660 100 C880 100 1100 292 1320 100 L1320 300 L0 300"
+              },
+              {
+                value:
+                  "M0 192 C220 100 440 100 660 192 C880 290 1100 290 1320 192 L1320 300 L0 300"
+              }
+            ]
+          });
+        }, []);
     }
 
 
@@ -53,10 +48,16 @@ export default function AboutMe(){
 
     return(
         <div className=" flex flex-col" id="aboutMe">
-
-<svg ref={svgRef} viewBox="0 0 1320 300" xmlns="http://www.w3.org/2000/svg">
-      <path fill="#000" d="M0 0h1320v300H0z" />
-      <path fill="#fff" d="M0 192 C220 100 440 100 660 192 C880 290 1100 290 1320 192 L1320 300 L0 300" />
+<svg
+      ref={svgRef}
+      viewBox="0 0 1320 300"
+      xmlns="http://www.w3.org/2000/svg"
+      style={{ backgroundColor: "black" }}
+    >
+      <path
+        fill="#d0d0d0"
+        d="M0 300 L0 192 C220 100 440 100 660 192 C880 290 1100 290 1320 192 L1320 300 Z"
+      ></path>
     </svg>
 
       
